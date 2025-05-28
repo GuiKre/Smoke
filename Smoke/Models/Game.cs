069e8c09e89bde9a74b61c77ee6c13d4b09967e3
Guilherme Krelling <guilhermekre@gmail.com>
2025-05-28 20:42:38 -0300

    DataAnnotations

--- Smoke/Models/Game.cs
+++ Smoke/Models/Game.cs
@@ -1,10 +1,21 @@
+using System.ComponentModel.DataAnnotations;
 namespace Smoke.Models
 {
     public class Game
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Este campo é obrigatório.")]
+        [MaxLength(100, ErrorMessage = "O nome precisa ter no máximo 100 caracteres.")]
         public String Nome { get; set; }
+
+        [Required(ErrorMessage = "Este campo é obrigatório.")]
+        [MaxLength(30, ErrorMessage = "O gênero precisa ter no máximo 30 caracteres.")]
         public String Genero { get; set; }
+
+        [Required(ErrorMessage = "Este campo é obrigatório.")]
+        [MaxLength(50, ErrorMessage = "O desenvolvedor precisa ter no máximo 50 caracteres.")]
         public String Desenvolvedor { get; set; }
     }
 }
