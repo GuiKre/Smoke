0b5f4eb59d7cac0c4d879094e2702a611db473d5
Guilherme Krelling <guilhermekre@gmail.com>
2025-05-28 21:16:55 -0300

    Atualização do README

--- Smoke/obj/Debug/net9.0/Smoke.AssemblyInfo.cs
+++ Smoke/obj/Debug/net9.0/Smoke.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Smoke")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a4ff3e10dedfa4d901aed159e7b9cb70e52fa5c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+069e8c09e89bde9a74b61c77ee6c13d4b09967e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Smoke")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Smoke")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
